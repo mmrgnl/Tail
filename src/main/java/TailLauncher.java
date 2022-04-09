513dece557935001ce36fbc1f596acb553827ff5
mmrgnl <m.p.max@live.com>
2022-04-09 22:07:24 +0300

    rubbish deleted

--- src/main/java/TailLauncher.java
+++ src/main/java/TailLauncher.java
@@ -1,22 +1,9 @@
-import java.io.*;
-
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.Files;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.ArrayList;
-
 public class TailLauncher {
-
-
-
 
     @Option (name = "-с",  usage = "Input last num of symbols")
     private Integer symbols;
@@ -27,55 +14,29 @@ public class TailLauncher {
     @Option (name = "-o",  usage = "Input fileOut Name")
     private String Out;
 
-
     @Argument( metaVar = "InputName", usage = "Input file name")
     private String fileN;
-
-
 
     public static void main(String[] args) {
         new TailLauncher().launch(args);
     }
 
-
-
-
-
     private void launch(String[] args) {
-        CmdLineParser parser = new CmdLineParser(this);
 
+        CmdLineParser parser = new CmdLineParser(this);
         try {
             parser.parseArgument(args);
         } catch (CmdLineException e) {
             System.err.println(e.getMessage());
-            System.err.println("java -jar recoder.jar -ie EncodingI -oe EncodingO InputName OutputName");
+            System.err.println("Command line: tail [-c num|-n num] [-o ofile] file");
             parser.printUsage(System.err);
             return;
         }
-
-
-
-
-
-
-
-
         Tail tail = new Tail(Out, symbols, bars);
-
         try {
-            tail.TailFound(tail, fileN);
+            Tail.TailFound(tail, fileN);
         } catch (Exception e) {
             System.err.println(e.getMessage());
-           // return;
         }
-
-
     }
-
-
-
-
-
-
-
 }
--- src/test/java/TailTest.java
+++ src/test/java/TailTest.java
@@ -1,78 +1,40 @@
-
-
 import java.io.*;
-
-import java.nio.charset.Charset;
-
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.apache.commons.io.FileUtils;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
 class TailTest {
-
-
-
-
-
-
-    //private fun
-
-    
-    //Tail Empty = new Tail("files/text1.txt", )
-
 
     @org.junit.jupiter.api.Test
 
     void TailFound(){
 
         Tail tail = new Tail("files/text2.txt", null, null);
-
         String in = "files/text1.txt";
-
         Tail.TailFound(tail, in);
-
         try {
             assertTrue(FileUtils.contentEquals(new File(tail.OutName), new File("files/test1.txt")),
                     "The files differ!");
        } catch (IOException e) {
             e.printStackTrace();
       }
-
-        new File(tail.OutName).delete();
-
         tail =  new Tail("files/text2.txt", 12, null);
-
         Tail.TailFound(tail, in);
-
         try {
             assertTrue(FileUtils.contentEquals(new File(tail.OutName), new File("files/test2.txt")),
                     "The files differ!");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        new File(tail.OutName).delete();
-
         tail = new Tail("files/text2.txt", null, 4);
-
         in = "files/text3.txt";
-
         Tail.TailFound(tail, in);
-
         try {
             assertTrue(FileUtils.contentEquals(new File(tail.OutName), new File("files/test3.txt")),
                     "The files differ!");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        new File(tail.OutName).delete();
     }
 
 }
